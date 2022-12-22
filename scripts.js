11f07c8331d2b8040853af4cb9b04b0a995fa06d
Lucia Moral <lmoralbaena@gmail.com>
2022-12-22 18:04:27 +0100

    installed parcel

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("works?")
+const baseEndpoint = "https://food-api-lgch.onrender.com/recipes";
+
+
+async function fetchRecipes(query) {
+  const res = await fetch(`${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  console.log(data);
+}
+
+fetchRecipes('pasta');
