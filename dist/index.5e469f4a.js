ed1212f11e8ccd1ba69a34778421ff28c4580ae9
Lucia Moral <lmoralbaena@gmail.com>
2022-12-26 09:31:43 +0100

    finished main functionality

---
+++ dist/index.5e469f4a.js
@@ -0,0 +1,32 @@
+const baseEndpoint = "https://food-api-lgch.onrender.com/recipes";
+const form = document.querySelector("form.search");
+const recipesGrid = document.querySelector(".recipes");
+async function fetchRecipes(query) {
+    const res = await fetch(`${baseEndpoint}?q=${query}`);
+    const data = await res.json();
+    return data;
+}
+async function handleSubmit(event) {
+    event.preventDefault();
+    const el = event.currentTarget;
+    console.log(form.query.value);
+    const recipes = await fetchRecipes(el.query.value);
+    displayRecipes(recipes);
+}
+function displayRecipes(recipes) {
+    console.log("Creating HTML...");
+    const html = recipes.map((recipe)=>{
+        return `<div class="recipe">
+      <h2>${recipe.name} </h2>
+      <p>${recipe.description} </p>
+      <p>${recipe.method} </p>
+      <img src="${recipe.url}" alt="${recipe.name}" />
+      <a href="${recipe.web}">View recipe </a>
+    </div>`;
+    });
+    recipesGrid.innerHTML = html.join("");
+}
+form.addEventListener("submit", handleSubmit);
+fetchRecipes("pizza");
+
+//# sourceMappingURL=index.5e469f4a.js.map
